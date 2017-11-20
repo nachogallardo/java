2def14e6ab653c4a47bc639b5604898b11088733
alumno1 <alumno1@alumno>
2017-11-20 08:44:57 +0100

    f

---
+++ hibernate02/src/main/java/es/altair/hibernate02/App.java
@@ -0,0 +1,13 @@
+package es.altair.hibernate02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
